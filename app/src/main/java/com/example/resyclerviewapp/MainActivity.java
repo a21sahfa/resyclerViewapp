9ba2a92e2e0956a4beacca657b4bad12c35c4fdf
a21sahfa <a21sahfa@student.his.se>
2022-04-06 14:51:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/resyclerviewapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.resyclerviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.resycler_view);
+        recyclerView.setAdapter(new MyAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
